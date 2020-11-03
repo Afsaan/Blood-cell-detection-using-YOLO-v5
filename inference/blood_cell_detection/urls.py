eeb5cbfa440667076152bf60f63371b9e3b26df0
afsan <khanafsaan11@gmail.com>
2020-11-03 11:09:42 +0530

    added url

--- inference/blood_cell_detection/urls.py
+++ inference/blood_cell_detection/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from .views import home
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', home),
