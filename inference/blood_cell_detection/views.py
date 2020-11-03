b2582b8c1e646c71efba882dc5b0b107286c2fb2
afsan <khanafsaan11@gmail.com>
2020-11-03 11:09:50 +0530

    added url

--- inference/blood_cell_detection/views.py
+++ inference/blood_cell_detection/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, HttpResponse
-from .models import FilesUpload
+from .models import File_Upload
 
 def home(request):
     if request.method == "POST":
         file2 = request.FILES["file"]
-        document = documents.objects.create(file = file2)
-        documen.save()
-        return HttpResponse("you file was uploaded")
+        document = File_Upload.objects.create(file = file2)
+        document.save()
+        context = {}
+        return render(request, "index.html", context)
     return render(request, "index.html")
+
+
+# def home(request):
+#     if request.method == "POST":
+#         file2 = request.FILES["file"]
+#         with open(f'{file2.name}.png','wb') as f:
+#             f.write(file2.read())
+#         # document = File_Upload.objects.create(file = file2)
+#         # document.save()
+#         # context = {}
+#         return render(request, "index.html",)
+#     return render(request, "index.html")
