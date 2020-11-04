45ab79b0c71a6180c1357f3ecee96dfa406db439
afsan <khanafsaan11@gmail.com>
2020-11-04 10:24:20 +0530

    version 2 -  now shoes the output

--- inference/blood_cell_detection/views.py
+++ inference/blood_cell_detection/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render, HttpResponse
 from .models import File_Upload
+from django.template import loader
+import os
 
 def home(request):
     if request.method == "POST":
         file2 = request.FILES["file"]
-        document = File_Upload.objects.create(file = file2)
-        document.save()
-        context = {}
-        return render(request, "index.html", context)
+        with open(f'media/{file2.name}','wb') as f:
+            f.write(file2.read())
+
+        os.system(f'python detect/detect.py --source media/{file2.name} --weights model_files/last.pt')
+        context = {'detected_image' : f'inference/detect/inference/output/{file2.name}'}
+        print(context)
+        # return render(request,'output.html')
+        template = loader.get_template('output.html')
+        context = {'detected_image' : f'static/inference/output/{file2.name}'}
+        response =  HttpResponse(template.render(context, request))
+        return response
+
     return render(request, "index.html")
 
 
-# def home(request):
-#     if request.method == "POST":
-#         file2 = request.FILES["file"]
-#         with open(f'{file2.name}.png','wb') as f:
-#             f.write(file2.read())
-#         # document = File_Upload.objects.create(file = file2)
-#         # document.save()
-#         # context = {}
-#         return render(request, "index.html",)
-#     return render(request, "index.html")
+
