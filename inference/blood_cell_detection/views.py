0d0a21abae8043c97d47cae8836e1c23274471a7
afsan <khanafsaan11@gmail.com>
2020-11-05 20:51:37 +0530

    version2

--- inference/blood_cell_detection/views.py
+++ inference/blood_cell_detection/views.py
@@ -6,15 +6,14 @@ import os
 def home(request):
     if request.method == "POST":
         file2 = request.FILES["file"]
-        with open(f'media/{file2.name}','wb') as f:
+        with open(f'static/sample/{file2.name}','wb') as f:
             f.write(file2.read())
 
-        os.system(f'python detect/detect.py --source media/{file2.name} --weights model_files/last.pt')
-        context = {'detected_image' : f'inference/detect/inference/output/{file2.name}'}
-        print(context)
+        os.system(f'python detect/detect.py --source static/sample/{file2.name} --weights model_files/last.pt')
         # return render(request,'output.html')
         template = loader.get_template('output.html')
-        context = {'detected_image' : f'static/inference/output/{file2.name}'}
+        context = {'detected_image' : f'static/inference/output/{file2.name}',
+                'normal_image' : f'static/sample/{file2.name}'}
         response =  HttpResponse(template.render(context, request))
         return response
 
